b3feb296325a29c49b17a060a3f071890c056b83
unknown <tlin41@dons.usfca.edu>
2021-03-17 13:11:06 -0700

    added total summary data.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,5 +8,12 @@ lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=
 
 summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=carData))
 
+#deliverable 2
+library(dplyr)
+library(tidyverse)
 
+suspensionData <- read.csv(file = 'Suspension_Coil.csv')
 
+lot_summary <- suspensionData %>% group_by(Manufacturing_Lot)%>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI) ,.groups = 'keep')
+
+total_summary <- suspensionData %>% group_by()
